8100ac66e5b11b1ed731928aa52971e000150a9f
Periyanayagam Santhappan <periyanayagam.santhappan@intellificial.com>
2025-06-18 16:01:54 +1000

    add data handling

--- tests/functional/login.pom.spec.ts
+++ tests/functional/login.pom.spec.ts
@@ -1,14 +1,27 @@
 import { test, expect } from "@playwright/test";
-import LoginPage from '../page-objects/login.page'
+import LoginPage from "../page-objects/login.page";
+import constants from "../../data/constants.json";
 
 test.describe("Login functionality", () => {
   test("Should login successfully", async ({ page }) => {
+    // Reading data from JSON
+    console.log(`>> Reading the JSON file: ${JSON.stringify(constants)}`);
+
     // Create a login page instance
     const loginPage = new LoginPage(page);
 
+    // Get env variables
+    const URL = process.env.URL;
+    const username = process.env.USER_NAME;
+    const password = process.env.PASSWORD;
+
+    if (!URL || !username || !password) {
+      throw new Error("Invalid creds passed..");
+    }
+
     // Launch the home page
-    await loginPage.launchHomePage("https://katalon-demo-cura.herokuapp.com/");
-    await loginPage.login("John Doe", "ThisIsNotAPassword");
+    await loginPage.launchHomePage(URL);
+    await loginPage.login(username, password);
   });
 
   // TODO: More tests to follow
